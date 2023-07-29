49cfddf28356aa7a47396f65409bbf91da908947
danilserov <danil.serov2010@yandex.ru>
2023-07-29 19:02:19 +0700

    chore: boost added

---
+++ ComUdpProxy/ComUdpProxy.cpp
@@ -0,0 +1,14 @@
+﻿// ComUdpProxy.cpp : Defines the entry point for the application.
+//
+
+#include "ComUdpProxy.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << "Hello ComUdpProxy." << endl;
+
+
+	return 0;
+}
---
+++ ComUdpProxy/ComUdpProxy.h
@@ -0,0 +1,8 @@
+﻿// ComUdpProxy.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
